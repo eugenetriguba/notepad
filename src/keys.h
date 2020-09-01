7eb8f1d9fe70eb9d13638a86d1fa6b3c95a3d824
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-31 20:12:44 -0500

    Accept -d and --debug for cli args debug mode

--- src/keys.h
+++ src/keys.h
@@ -1,6 +1,6 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k)&0x1f)
 
 #endif /* KEYS_H */
--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     notepad_t *notepad = notepad_create(STDIN_FILENO);
 
     for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "-d") == 0) {
+        if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--debug") == 0)) {
             notepad->debug_mode = 1;
             printf("Notepad debug mode is on.\r\n");
         }
