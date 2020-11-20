e6272664fa04eae38eb71984bf042f39e26d7bbf
Eugene Triguba <eugenetriguba@gmail.com>
2020-11-20 11:05:56 -0600

    Update tests for ctrl key

--- tests/test_notepad.c
+++ tests/test_notepad.c
@@ -15,11 +15,13 @@
 #include "../src/keys.h"
 #include "helpers.h"
 
-// src/notepad.c: startup_notepad_app
-//
-// Ensure that the startup_notepad_app's
-// return value is 0, since this is what
-// main() will use as it's exit code.
+/**
+ * src/notepad.c: startup_notepad_app
+ *
+ * Ensure that the startup_notepad_app's
+ * return value is 0, since this is what
+ * main() will use as it's exit code.
+ */
 void test_startup_notepad_app_return_value() {
     char contents[4];
     sprintf(contents, "%d", CTRL_KEY('q'));
@@ -28,14 +30,18 @@ void test_startup_notepad_app_return_value() {
     assert_true(startup_notepad_app(notepad) == 0);
 }
 
-// src/notepad.c: read_all_from_fd
-//
-// Ensure that the contents in the file descriptor
-// are read into the notepad's contents, excluding
-// the quit 'q' command.
+/**
+ * src/notepad.c: read_all_from_fd
+ *
+ * Ensure that the contents in the file descriptor
+ * are read into the notepad's contents, excluding
+ * the quit 'q' command.
+ */
 void test_contents_in_notepad_include_fd_contents() {
     char *contents = "aaa";
-    notepad_t *notepad = create_test_notepad("aaaq");
+    sprintf(contents, "%d", CTRL_KEY('q'));
+
+    notepad_t *notepad = create_test_notepad(contents);
 
     read_all_from_fd(notepad);
 
--- tests/test_terminal.c
+++ tests/test_terminal.c
@@ -15,18 +15,20 @@
 
 void assert_equal_term_flags(struct termios t1, struct termios t2) {
     assert_true(t1.c_oflag == t2.c_oflag);
-    // assert_true(t1.c_lflag == t2.c_lflag);
-    // assert_true(t1.c_cflag == t2.c_cflag);
-    // assert_true(t1.c_iflag == t2.c_iflag);
+    assert_true(t1.c_lflag == t2.c_lflag);
+    assert_true(t1.c_cflag == t2.c_cflag);
+    assert_true(t1.c_iflag == t2.c_iflag);
     assert_true(t1.c_cc[VMIN] == t2.c_cc[VMIN]);
     assert_true(t1.c_cc[VTIME] == t2.c_cc[VTIME]);
 }
 
-// src/terminal.c: terminal_create
-//
-// The terminal_t from terminal_create() should
-// have an original_settings member that has the
-// termios settings of the current terminal.
+/**
+ * src/terminal.c: terminal_create
+ *
+ * The terminal_t from terminal_create() should
+ * have an original_settings member that has the
+ * termios settings of the current terminal.
+ */
 void test_terminal_create_has_original_terminal_settings() {
     terminal_t *term = create_test_notepad("q")->terminal;
     struct termios current_term;
